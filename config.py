f8c15c0f48e5d625d6b273bb87b5dcdfd51136f4
Abhijeet-Singh14 <102838018+Abhijeet-Singh14@users.noreply.github.com>
2023-11-29 15:58:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 import logging
 class Config:                                                                   
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")       
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", "20762333"))
+    API_HASH = os.environ.get("API_HASH", "97bb560b7e50f706d2ae6e4bd7caed89")       
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6862953115:AAF-7MnyallqF65rckSoWY0t3xYqhsPb0IQ")
     BOT_SESSION = os.environ.get("BOT_SESSION", "forwardbot")
-    OWNER_ID = os.environ.get("OWNER_ID", "")                             
+    OWNER_ID = os.environ.get("OWNER_ID", "664880413")                             
     DATABASE_URI = os.environ.get("DATABASE_URI", "")  
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluste0")
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Data')
